4e1e7d57abd620e7913cab6a993ee083d32d3e5c
Iivari Äikäs <iivari.aikas@gmail.com>
2014-03-26 12:41:54 +0200

    Canto settings

--- canto/conf.py
+++ canto/conf.py
@@ -1,4 +1,11 @@
+from canto.extra import *
 from os.path import expanduser
 
 opml_path = expanduser("~/.canto/feeds.opml")
 source_opml(opml_path)
+
+link_handler("firefox \"%u\"")
+
+reader_keys['j'] = "scroll_down"
+reader_keys['k'] = "scroll_up"
+
