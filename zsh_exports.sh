ada7ed5433890e6b4b05679dd63bee9486933307
Iivari Äikäs <iivari.aikas@gmail.com>
2019-11-10 21:45:36 +0200

    Check that binaries exist before exporting them

--- zsh_exports.sh
+++ zsh_exports.sh
@@ -1,14 +1,20 @@
 export PATH=$PATH:$HOME/bin
 export BROWSER=firefox
 
-if [ -n "$DESKTOP_SESSION" ];then
+if [[ -n "$DESKTOP_SESSION" ]]; then
+  if hash gnome-keyring-daemon 2> /dev/null; then
     eval $(gnome-keyring-daemon --start)
     export SSH_AUTH_SOCK
+  fi
 fi
 
 if [[ -n $SSH_CONNECTION ]]; then
   export EDITOR='vim'
 else
-  export EDITOR='vim'
+  if hash nvim 2> /dev/null; then
+    export EDITOR='nvim'
+  else
+    export EDITOR='vim'
+  fi
 fi
 
