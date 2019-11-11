607419576739b0978f8d3b8b8dfcfa31944049e2
Iivari Äikäs <iivari.aikas@gmail.com>
2019-11-11 19:50:38 +0200

    Don't try to launch keyring if we have $SSH_AUTH_SOCK

--- zsh_exports.sh
+++ zsh_exports.sh
@@ -1,7 +1,7 @@
 export PATH=$PATH:$HOME/bin
 export BROWSER=firefox
 
-if [[ -n "$DESKTOP_SESSION" ]]; then
+if [[ -n "$DESKTOP_SESSION" && -z "$SSH_AUTH_SOCK" ]]; then
   if hash gnome-keyring-daemon 2> /dev/null; then
     eval $(gnome-keyring-daemon --start)
     export SSH_AUTH_SOCK
