1cd4e105476f0f4ba843574217c1c3c5bcbf80c7
Iivari Äikäs <iivari.aikas@reaktor.com>
2020-01-07 10:25:32 +0200

    Workhorse stuff

--- xdg_config/sway/statusbar.sh
+++ xdg_config/sway/statusbar.sh
@@ -7,11 +7,16 @@ set -o pipefail
 HOST=$(cat /etc/hostname)
 
 while true; do
-    if [[ $HOST = "papaya" ]]; then
+    if [[ $HOST = "workhorse" ]]; then
+        DATE=$(date +'%a %Y-%m-%d - %k:%M')
+        BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+        echo "BAT: $BAT% | $DATE "
+        sleep 15
+    elif [[ $HOST = "papaya" ]]; then
         DATE=$(date +'%a %Y-%m-%d - %k:%M')
         BAT=$(cat /sys/class/power_supply/BAT1/capacity)
         echo "BAT: $BAT% | $DATE "
-        sleep 3
+        sleep 15
     elif [[ $HOST = "kisubox" ]]; then
         DATE=$(date +'%a %Y-%m-%d - %k:%M:%S ')
         echo "$DATE"
