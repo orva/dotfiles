b5c1a8799e9d0939c9b04dd3fb2f78f6372d2616
Iivari Äikäs <iivari.aikas@gmail.com>
2020-02-28 14:07:34 +0200

    Move zsh setup scripts into own directory
    Also use .zsh extension to get editors/shellcheck to recognize shell.

--- zsh_setup/powerlevel10k.zsh
+++ zsh_setup/powerlevel10k.zsh
@@ -5,6 +5,7 @@ fi
 
 # shellcheck source=/dev/null
 source "$P10K_DIR/powerlevel10k.zsh-theme"
+unset P10K_DIR
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
@@ -14,8 +15,9 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.dotfiles/p10k.zsh ]] || source ~/.dotfiles/p10k.zsh
+P10K_USR_CONF="$HOME/.dotfiles/zsh_setup/p10k.zsh"
+[[ ! -f "$P10K_USR_CONF" ]] || source "$P10K_USR_CONF"
+unset P10K_USR_CONF
 
 alias ls='ls --color=auto'
 setopt HIST_IGNORE_SPACE
-unset P10K_DIR
