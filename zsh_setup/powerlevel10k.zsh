4cc41bfb252964c66c1003e3df51ec910fba396c
Iivari Äikäs <iivari.aikas@gmail.com>
2020-04-25 12:20:40 +0300

    Generate p10k user config for every machine
    Seems like setup does some compatibility things and thus configs are not
    shareable between machines.

--- zsh_setup/powerlevel10k.zsh
+++ zsh_setup/powerlevel10k.zsh
@@ -10,12 +10,12 @@ unset P10K_DIR
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
+# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+# fi
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-P10K_USR_CONF="$HOME/.dotfiles/zsh_setup/p10k.zsh"
+P10K_USR_CONF="$HOME/.p10k.zsh"
 [[ ! -f "$P10K_USR_CONF" ]] || source "$P10K_USR_CONF"
 unset P10K_USR_CONF
 
