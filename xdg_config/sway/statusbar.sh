c38728f55c454af0e9d2dcc66cd4b8419387d99d
Iivari Äikäs <iivari.aikas@reaktor.com>
2021-03-21 12:16:39 +0200

    Add network manager connections to statusbar script

--- xdg_config/sway/statusbar.sh
+++ xdg_config/sway/statusbar.sh
@@ -8,15 +8,43 @@ HOST=$(cat /etc/hostname)
 CONF="$HOME/.config/swaybar-data/$HOST.toml"
 GENERATOR="$HOME/bin/swaybar-data"
 
-if [[ $HOST = "workhorse" ]]; then
-    while true; do
-        DATE=$(date +'%a %Y-%m-%d - %k:%M')
-        BAT=$(cat /sys/class/power_supply/BAT0/capacity)
-        echo "BAT: $BAT% | $DATE "
-        sleep 15
-    done
-elif [[ $HOST = "papaya" ]]; then
+if [[ $HOST = "papaya" ]]; then
     $GENERATOR --config="$CONF"
 elif [[ $HOST = "kisubox" ]]; then
     $GENERATOR --config="$CONF"
+else
+    while true; do
+        DATE=$(date +'%a %Y-%m-%d - %k:%M')
+        BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+
+        CONNECTIONS=""
+        NMCLI_OUT=$(nmcli --terse --fields 'state,type,connection' device)
+        while read -r CONN_LINE
+        do
+          STATE=""
+          TYPE=""
+          CONN=""
+          IFS=: read -r STATE TYPE CONN <<< "$CONN_LINE"
+
+          if [ "$STATE" != "connected" ]; then
+            continue
+          fi
+
+          STATUS=""
+          if [ "$TYPE" = "ethernet" ]; then
+            STATUS="$TYPE"
+          else
+            STATUS="$TYPE:$CONN"
+          fi
+
+          if [ "$CONNECTIONS" = "" ]; then
+            CONNECTIONS="$STATUS"
+          else
+            CONNECTIONS="${CONNECTIONS} | ${STATUS}"
+          fi
+        done <<< "$NMCLI_OUT"
+
+        echo "${CONNECTIONS} | BAT: ${BAT}% | ${DATE} "
+        sleep 5
+    done
 fi
