6a4eb2827311be7d9afd586cd6caf1a2cd89cd2e
Iivari Äikäs <iivari.aikas@gmail.com>
2023-11-27 11:38:20 +0200

    Enable powerlevel10k instant prompt

--- zsh_setup/powerlevel10k.zsh
+++ zsh_setup/powerlevel10k.zsh
@@ -10,9 +10,9 @@ unset P10K_DIR
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
-# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-# fi
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 P10K_USR_CONF="$HOME/.p10k.zsh"
