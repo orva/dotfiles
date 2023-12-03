09af12dbe5abded666bc92ba8f27bb38cc5e7632
Iivari Äikäs <iivari.aikas@gmail.com>
2023-12-03 17:09:32 +0200

    Use trouble for diagnostics navigation

--- xdg_config/nvim/lua/plugins/diagnostics.fnl
+++ xdg_config/nvim/lua/plugins/diagnostics.fnl
@@ -1,14 +1,24 @@
 (local {: dep-spec } (require :util))
 
 (fn config []
-  (let [ls (require :null-ls)]
+  (let [ls (require :null-ls)
+        trouble (require :trouble)]
     (ls.setup {:sources [(. ls :builtins :diagnostics :eslint)
                          (. ls :builtins :diagnostics :clj_kondo)
                          (. ls :builtins :formatting :biome)
                          (. ls :builtins :formatting :zprint)
-                         (. ls :builtins :formatting :rustfmt)]})))
+                         (. ls :builtins :formatting :rustfmt)]})
+
+        (vim.keymap.set :n "<leader>xx" trouble.toggle)
+        (vim.keymap.set :n "<leader>xw" #(trouble.toggle :workspace_diagnostics))
+        (vim.keymap.set :n "<leader>xd" #(trouble.toggle :document_diagnostics))
+        (vim.keymap.set :n "<leader>xq" #(trouble.toggle :quickfix))
+        (vim.keymap.set :n "<leader>xl" #(trouble.toggle :locklist))
+        (vim.keymap.set :n "<leader>xr" #(trouble.toggle :lsp_references))))
 
 ; null-ls renamed to none-ls
 (dep-spec "nvimtools/none-ls.nvim"
-          {:dependencies ["nvim-lua/plenary.nvim"]
+          {:dependencies ["nvim-lua/plenary.nvim"
+                          "folke/trouble.nvim"
+                          "nvim-tree/nvim-web-devicons"]
            : config})
--- xdg_config/nvim/lua/plugins/diagnostics.lua
+++ xdg_config/nvim/lua/plugins/diagnostics.lua
@@ -3,6 +3,28 @@ local _local_1_ = require("util")
 local dep_spec = _local_1_["dep-spec"]
 local function config()
   local ls = require("null-ls")
-  return ls.setup({sources = {ls.builtins.diagnostics.eslint, ls.builtins.diagnostics.clj_kondo, ls.builtins.formatting.biome, ls.builtins.formatting.zprint, ls.builtins.formatting.rustfmt}})
+  local trouble = require("trouble")
+  ls.setup({sources = {ls.builtins.diagnostics.eslint, ls.builtins.diagnostics.clj_kondo, ls.builtins.formatting.biome, ls.builtins.formatting.zprint, ls.builtins.formatting.rustfmt}})
+  vim.keymap.set("n", "<leader>xx", trouble.toggle)
+  local function _2_()
+    return trouble.toggle("workspace_diagnostics")
+  end
+  vim.keymap.set("n", "<leader>xw", _2_)
+  local function _3_()
+    return trouble.toggle("document_diagnostics")
+  end
+  vim.keymap.set("n", "<leader>xd", _3_)
+  local function _4_()
+    return trouble.toggle("quickfix")
+  end
+  vim.keymap.set("n", "<leader>xq", _4_)
+  local function _5_()
+    return trouble.toggle("locklist")
+  end
+  vim.keymap.set("n", "<leader>xl", _5_)
+  local function _6_()
+    return trouble.toggle("lsp_references")
+  end
+  return vim.keymap.set("n", "<leader>xr", _6_)
 end
-return dep_spec("nvimtools/none-ls.nvim", {dependencies = {"nvim-lua/plenary.nvim"}, config = config})
+return dep_spec("nvimtools/none-ls.nvim", {dependencies = {"nvim-lua/plenary.nvim", "folke/trouble.nvim", "nvim-tree/nvim-web-devicons"}, config = config})
