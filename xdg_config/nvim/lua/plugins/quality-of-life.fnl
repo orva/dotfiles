7876aed4e1cdceddae9fbb3633f9e4e601978814
Iivari Äikäs <iivari.aikas@gmail.com>
2024-02-14 14:20:27 +0200

    Actually setup guess-indent plugin

--- xdg_config/nvim/lua/plugins/quality-of-life.fnl
+++ xdg_config/nvim/lua/plugins/quality-of-life.fnl
@@ -1,3 +1,8 @@
 (local {: dep-spec} (require :util))
 
-[(dep-spec :tpope/vim-sensible) (dep-spec :NMAC427/guess-indent.nvim)]
+(fn guess-indent-setup []
+  (let [guess-indent (require :guess-indent)]
+    (guess-indent.setup)))
+
+[(dep-spec :tpope/vim-sensible)
+ (dep-spec :NMAC427/guess-indent.nvim {:config guess-indent-setup})]
--- xdg_config/nvim/lua/plugins/quality-of-life.lua
+++ xdg_config/nvim/lua/plugins/quality-of-life.lua
@@ -1,4 +1,8 @@
 -- [nfnl] Compiled from lua/plugins/quality-of-life.fnl by https://github.com/Olical/nfnl, do not edit.
 local _local_1_ = require("util")
 local dep_spec = _local_1_["dep-spec"]
-return {dep_spec("tpope/vim-sensible"), dep_spec("NMAC427/guess-indent.nvim")}
+local function guess_indent_setup()
+  local guess_indent = require("guess-indent")
+  return guess_indent.setup()
+end
+return {dep_spec("tpope/vim-sensible"), dep_spec("NMAC427/guess-indent.nvim", {config = guess_indent_setup})}
