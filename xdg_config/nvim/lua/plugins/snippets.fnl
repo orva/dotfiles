6ce3fe203dc61ae5d42737a62c099fd74ee5d017
Iivari Äikäs <iivari.aikas@gmail.com>
2024-02-14 11:05:12 +0200

    Config LuaSnip to use vscode snippets

--- xdg_config/nvim/lua/plugins/snippets.fnl
+++ xdg_config/nvim/lua/plugins/snippets.fnl
@@ -1,2 +1,11 @@
 (local {: dep-spec} (require :util))
-(dep-spec :L3MON4D3/LuaSnip {:version :v2.*})
+
+(fn config []
+  (let [loader (require :luasnip.loaders.from_vscode)]
+    (loader:lazy_load)))
+
+(dep-spec :L3MON4D3/LuaSnip {:name :snippets
+                             :version :v2.*
+                             :dependencies [:rafamadriz/friendly-snippets]
+                             :build "make install_jsregexp"
+                             : config})
--- xdg_config/nvim/lua/plugins/snippets.lua
+++ xdg_config/nvim/lua/plugins/snippets.lua
@@ -1,4 +1,8 @@
 -- [nfnl] Compiled from lua/plugins/snippets.fnl by https://github.com/Olical/nfnl, do not edit.
 local _local_1_ = require("util")
 local dep_spec = _local_1_["dep-spec"]
-return dep_spec("L3MON4D3/LuaSnip", {version = "v2.*"})
+local function config()
+  local loader = require("luasnip.loaders.from_vscode")
+  return loader:lazy_load()
+end
+return dep_spec("L3MON4D3/LuaSnip", {name = "snippets", version = "v2.*", dependencies = {"rafamadriz/friendly-snippets"}, build = "make install_jsregexp", config = config})
