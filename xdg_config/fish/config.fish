adb7f526a58033e81d81929fc197ee0fcf7a0728
Iivari Äikäs <iivari.aikas@gmail.com>
2024-02-26 10:55:22 +0200

    Fool around with fish

---
+++ xdg_config/fish/config.fish
@@ -0,0 +1,8 @@
+# some useful variables to set:
+#
+# set -Ux SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
+# set -Ux EDITOR nvim
+
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+end
