d7bb4b201ee6b52e15c966c7f6467e40253cb7ae
Iivari Äikäs <iivari.aikas@gmail.com>
2024-03-01 20:08:22 +0200

    Add reminder to use bat for manpages

--- xdg_config/fish/config.fish
+++ xdg_config/fish/config.fish
@@ -2,6 +2,9 @@
 #
 # set -Ux SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
 # set -Ux EDITOR nvim
+# set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
+# set -Ux MANROFFOPT -c
+
 
 if status is-interactive
   # Commands to run in interactive sessions can go here
