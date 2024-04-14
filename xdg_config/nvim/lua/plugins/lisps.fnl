8382617314cb493fd27eeb0ec09bcf09c7f43017
Iivari Äikäs <iivari.aikas@gmail.com>
2024-04-14 16:30:35 +0300

    Prefer locally installed fennel is present

--- xdg_config/nvim/lua/plugins/lisps.fnl
+++ xdg_config/nvim/lua/plugins/lisps.fnl
@@ -1,9 +1,41 @@
 (local {: dep-spec} (require :util))
 
+(local project-markers [".*\\.rockspec" :lua_modules "\\.git"])
+(fn fennel-project? [name _path]
+  (not (vim.tbl_isempty (vim.tbl_filter #(name:match $) project-markers))))
+
+(fn find-local-fennel [path]
+  (let [[local-fennel] (vim.fs.find :fennel {: path})]
+    (or local-fennel :fennel)))
+
+(fn find-fennel []
+  (let [[project-root] (vim.fs.find fennel-project?
+                                    {:upward true
+                                     :stop (vim.fn.getcwd)
+                                     :path (-> (vim.api.nvim_buf_get_name 0)
+                                               vim.fs.dirname)})]
+    (if project-root
+        (find-local-fennel project-root) :fennel)))
+
 [(dep-spec :Olical/conjure
-           {:init (fn []
+           {:config (fn []
+                      ;; TODO: prevent automic REPL open and add own autocmd to
+                      ;;       do this this after opening fennel file.
+                      (set vim.g.conjure#client#fennel#stdio#command
+                           (find-fennel)))
+            :init (fn []
                     (set vim.g.conjure#filetypes
-                         [:clojure :fennel :janet :hy :julia :racket :scheme :lua :lisp :python :sql])
+                         [:clojure
+                          :fennel
+                          :janet
+                          :hy
+                          :julia
+                          :racket
+                          :scheme
+                          :lua
+                          :lisp
+                          :python
+                          :sql])
                     (set vim.g.conjure#filetype#fennel
                          :conjure.client.fennel.stdio))})
  (dep-spec :tpope/vim-sexp-mappings-for-regular-people
--- xdg_config/nvim/lua/plugins/lisps.lua
+++ xdg_config/nvim/lua/plugins/lisps.lua
@@ -1,13 +1,38 @@
 -- [nfnl] Compiled from lua/plugins/lisps.fnl by https://github.com/Olical/nfnl, do not edit.
 local _local_1_ = require("util")
 local dep_spec = _local_1_["dep-spec"]
-local function _2_()
+local project_markers = {".*\\.rockspec", "lua_modules", "\\.git"}
+local function fennel_project_3f(name, _path)
+  local function _2_(_241)
+    return name:match(_241)
+  end
+  return not vim.tbl_isempty(vim.tbl_filter(_2_, project_markers))
+end
+local function find_local_fennel(path)
+  local _let_3_ = vim.fs.find("fennel", {path = path})
+  local local_fennel = _let_3_[1]
+  return (local_fennel or "fennel")
+end
+local function find_fennel()
+  local _let_4_ = vim.fs.find(fennel_project_3f, {upward = true, stop = vim.fn.getcwd(), path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))})
+  local project_root = _let_4_[1]
+  if project_root then
+    return find_local_fennel(project_root)
+  else
+    return "fennel"
+  end
+end
+local function _6_()
+  vim.g["conjure#client#fennel#stdio#command"] = find_fennel()
+  return nil
+end
+local function _7_()
   vim.g["conjure#filetypes"] = {"clojure", "fennel", "janet", "hy", "julia", "racket", "scheme", "lua", "lisp", "python", "sql"}
   vim.g["conjure#filetype#fennel"] = "conjure.client.fennel.stdio"
   return nil
 end
-local function _3_()
+local function _8_()
   vim.g.sexp_filetypes = "clojure,scheme,lisp,timl,fennel,janet"
   return nil
 end
-return {dep_spec("Olical/conjure", {init = _2_}), dep_spec("tpope/vim-sexp-mappings-for-regular-people", {dependencies = {"guns/vim-sexp", "tpope/vim-repeat"}, init = _3_}), dep_spec("tpope/vim-surround")}
+return {dep_spec("Olical/conjure", {config = _6_, init = _7_}), dep_spec("tpope/vim-sexp-mappings-for-regular-people", {dependencies = {"guns/vim-sexp", "tpope/vim-repeat"}, init = _8_}), dep_spec("tpope/vim-surround")}
