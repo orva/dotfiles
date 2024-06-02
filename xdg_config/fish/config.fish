7831eded06a7f7d20e8165f38d1c5aedd4127767
Iivari Äikäs <iivari.aikas@gmail.com>
2024-06-02 13:07:28 +0300

    ssh-agent configurations for plasma

--- xdg_config/fish/config.fish
+++ xdg_config/fish/config.fish
@@ -1,6 +1,11 @@
 # some useful variables to set:
 #
+# For gnome and `systemctl --user enable gcr-ssh-agent.socket`
 # set -Ux SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
+#
+# For kde and `systemctl --user enable ssh-agent`
+# set -Ux SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
+#
 # set -Ux EDITOR nvim
 # set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
 # set -Ux MANROFFOPT -c
