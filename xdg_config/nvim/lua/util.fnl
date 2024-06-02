38b32f80835e2de9c9a53d71e0d577ad233e1599
Iivari Äikäs <iivari.aikas@gmail.com>
2024-06-02 13:13:03 +0300

    Simplify dep-spec util

--- xdg_config/nvim/lua/util.fnl
+++ xdg_config/nvim/lua/util.fnl
@@ -1,7 +1,7 @@
 (local core (require :nfnl.core))
 
-(fn dep-spec [name maybe-opts]
-  (let [opts (if (not= nil maybe-opts) maybe-opts {})]
+(fn dep-spec [name opts?]
+  (let [opts (or opts? {})]
     (core.assoc opts 1 name)))
 
 {: dep-spec}
--- xdg_config/nvim/lua/util.lua
+++ xdg_config/nvim/lua/util.lua
@@ -1,12 +1,7 @@
 -- [nfnl] Compiled from lua/util.fnl by https://github.com/Olical/nfnl, do not edit.
 local core = require("nfnl.core")
-local function dep_spec(name, maybe_opts)
-  local opts
-  if (nil ~= maybe_opts) then
-    opts = maybe_opts
-  else
-    opts = {}
-  end
+local function dep_spec(name, opts_3f)
+  local opts = (opts_3f or {})
   return core.assoc(opts, 1, name)
 end
 return {["dep-spec"] = dep_spec}
